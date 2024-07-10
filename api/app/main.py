564b7fc0f8fe60e13cd77ac775864b78ea33fd3e
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-09 20:56:44 -0400

    Init db at start

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import Depends, FastAPI
-
+from database.database import SessionLocal, engine, Base,db
 from .routers import auth
-
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(auth.router)
+
+def get_db():
+    try:
+        yield db
+    finally:
+        db.close()
