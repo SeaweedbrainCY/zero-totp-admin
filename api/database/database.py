27387b64ec28e5acefc3f9d9f50a0cb12c020c3e
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-09 20:56:19 -0400

    Add database definition

---
+++ api/database/database.py
@@ -0,0 +1,14 @@
+from environment.environment import conf
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from zero_totp_db_model.model_init import init_db
+
+
+engine = create_engine(
+    conf.database.zero_totp_db_uri
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db = SessionLocal()
+Base = declarative_base()
+init_db(db)
