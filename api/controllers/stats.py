f5e838ab4ffd0423868aad396fe1686cf31bc617
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-11 00:11:04 -0400

    Add basic stats retrieval

--- api/controllers/stats.py
+++ api/controllers/stats.py
@@ -1,8 +1,46 @@
 from flask import request, Response, redirect, make_response
-import flask
-import connexion
+from database.repositories import user as user_repo
+import datetime as dt
 
 
 
 def get_users_stats():
-    return Response("Not implemented", status=501)
+    users = user_repo.get_all_users()
+    total_users = len(users)
+    verified_users = len([user for user in users if user.isVerified])
+    blocked_users = len([user for user in users if user.isBlocked])
+    first_date_to_count = (dt.datetime.now() - dt.timedelta(days=365))
+    new_users_per_months = {}
+    for user in users:
+        creation_date = dt.datetime.strptime(user.createdAt, "%d/%m/%Y")
+        if creation_date < first_date_to_count:
+            creation_month = first_date_to_count.strftime("%B %Y")
+        else:
+            creation_month = creation_date.strftime("%B %Y")
+        if creation_month not in new_users_per_months:
+            new_users_per_months[creation_month] = 0
+        new_users_per_months[creation_month] += 1
+    months_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    total_user_per_months = {}
+    first_mount_i = months_list.index(first_date_to_count.strftime("%B"))
+    new_users_months = new_users_per_months.keys()
+    if first_date_to_count.strftime("%B") in new_users_months:
+        total_user_per_months[first_date_to_count.strftime("%B %Y")] = new_users_per_months[first_date_to_count.strftime("%B %Y")]
+    else:
+        total_user_per_months[first_date_to_count.strftime("%B %Y")] = 0
+    for i in range(1,13):
+        current_year = first_date_to_count.year if first_date_to_count.month + i <= 12 else first_date_to_count.year + 1
+        previous_month_year = first_date_to_count.year if first_date_to_count.month + i - 1 <= 12 else first_date_to_count.year + 1
+        month = months_list[(first_mount_i + i) % 12] + " " + str(current_year)
+        if month in new_users_months:
+            total_user_per_months[month] = new_users_per_months[month] + total_user_per_months[months_list[(first_mount_i + i - 1) % 12] + " " + str(previous_month_year)]
+        else:
+            total_user_per_months[month] = total_user_per_months[months_list[(first_mount_i + i - 1) % 12] + " " + str(previous_month_year)]
+
+
+    return {
+        "total_users": total_users,
+        "verified_users": verified_users,
+        "blocked_users": blocked_users,
+        "total_user_per_month_over_year": total_user_per_months
+    }
---
+++ api/database/repositories/user.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from zero_totp_db_model.model import User
+from database.db import db
+
+
+def get_all_users():
+    return db.session.query(User).all()
