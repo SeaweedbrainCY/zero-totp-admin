ce45fd39e239095afd7f211f7fc26c7c5bb4f384
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-14 23:08:16 -0400

    Add interconnection with api to retrieve stat

--- frontend/src/app/overview/overview.component.ts
+++ frontend/src/app/overview/overview.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
-import { faChartLine, faUsersRectangle, faServer } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine, faUsersRectangle, faServer, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import { NgChartsConfiguration } from 'ng2-charts';
 import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
   faUsersRectangle=faUsersRectangle
   faChartLine = faChartLine;
   faServer=faServer;
+  faCircleNotch=faCircleNotch;
+  total_user:number|undefined;
+  total_verified_user:number|undefined;
+  total_blocked_user:number|undefined;
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: [
       '01/01/2024',
@@ -38,6 +45,38 @@ export class OverviewComponent {
   };
   public lineChartLegend = true;
 
-  constructor() { }
+
+  constructor(
+    private http: HttpClient
+
+  ) { 
+  }
+
+  ngOnInit(): void {
+   this.getStats();
+  }
+
+
+  private getStats() {
+    this.http.get("/api/v1/stats/users",  {withCredentials:true, observe: 'response'}).subscribe((response) => {
+      if (response.status === 200) {
+        const body = JSON.parse(JSON.stringify(response.body));
+        console.log(body)
+        this.total_user = body.total_users;
+        this.total_verified_user = body.verified_users;
+        this.total_blocked_user = body.blocked_users;
+        console.log(this.total_blocked_user)
+      }
+  }, (error) => {
+    console.error(error);
+  });
+  }
+
+  public sortByDate(array:Array<string>): void {
+    array.sort((a,b) => {
+        return Date.parse(a) - Date.parse(b) ;
+
+    });
+}
 
 }
