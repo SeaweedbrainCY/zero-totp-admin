b549c8b252f7b56f9981d3d04069ccb42b30b99c
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-15 22:35:12 -0400

    Add repo to get rate limiting stats

---
+++ api/database/repositories/rate_limiting.py
@@ -0,0 +1,9 @@
+from zero_totp_db_model.model import RateLimiting
+from database.db import db
+
+
+def get_rate_limited_ip():
+    return db.session.query(RateLimiting).filter_by(action_type="failed_login").all()
+
+def get_rate_limited_emails():
+    return db.session.query(RateLimiting).filter_by(action_type="send_verification_email").all()
