485e46eacdb61336e96c1cdeff49bdacfc7c99a7
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-20 21:37:13 -0400

    Add get user by id repo

--- api/admin_database/repositories/admin_repo.py
+++ api/admin_database/repositories/admin_repo.py
@@ -3,5 +3,8 @@ from ..models import Admin
 from main import db
 
 
-def get_user_by_username(id):
-    return db.session.query(Admin).filter(Admin.username == id).first()
+def get_user_by_username(username):
+    return db.session.query(Admin).filter(Admin.username == username).first()
+
+def get_user_by_id(id):
+     return db.session.query(Admin).filter(Admin.id == id).first()
