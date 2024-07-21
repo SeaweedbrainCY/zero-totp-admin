57660e27f30da6a698cad0b54150fa60b174267d
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-20 21:37:59 -0400

    Add controller for whoami

--- api/controllers/auth.py
+++ api/controllers/auth.py
@@ -1,16 +1,18 @@
-from flask import request
+from flask import request, Response
 from hashlib import scrypt
-from admin_database.repositories.admin_repo import get_user_by_username
+from admin_database.repositories import admin_repo
+from admin_database.repositories import session as session_repo
 import os
 from base64 import b64encode, b64decode
 from environment.configuration import conf
+import json
 
 def login():
     username = request.json.get("username")
     password = request.json.get("password")
     if not username or not password:
         return {"error": "Missing username or password"}, 400
-    user = get_user_by_username(username)
+    user = admin_repo.get_user_by_username(username)
     if not user:
         #fight against timing attacks
         scrypt(password.encode(), salt=os.urandom(16), n=conf.security.scrypt_n, r=conf.security.scrypt_r, p=conf.security.scrypt_p)
@@ -23,4 +25,16 @@ def login():
         if hashed_pass != user.password:
             return {"error": "Invalid username or password"}, 403
         else:
-            return {"message": "Login successful"}, 200
+            session_id = session_repo.create_session(user.id)
+            if not session_id:
+                return {"error": "Internal server error"}, 500
+            response = Response(status=200, mimetype="application/json", response=json.dumps({"status":"OK"}))
+            response.set_cookie("session_id", session_id, httponly=True, secure=True, samesite="strict", max_age=conf.api.session_ttl)
+            return response
+
+def whoami(user):
+    user_obj = admin_repo.get_user_by_id(user)
+    if not user_obj:
+        return {"error": "User not found"}, 401
+    
+    return {"username": user_obj.username}, 200
