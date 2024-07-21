3c41f6e7cf8cfa61f381a16dc03dab3d2195c6a5
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-20 20:30:57 -0400

    Add new session table

--- api/admin_database/models.py
+++ api/admin_database/models.py
@@ -8,3 +8,10 @@ class Admin(db.Model):
     password = db.Column(db.String(255), nullable=False)
     password_salt = db.Column(db.String(255), nullable=False)
 
+class Session(db.Model):
+    __tablename__ = 'session'
+    __bind_key__ = "admin_db"
+    id = db.Column(db.String(36), primary_key=True)
+    user_id = db.Column(db.String(36), db.ForeignKey('admin.id'), nullable=False)
+    expiration = db.Column(db.Integer, nullable=False)
+    
