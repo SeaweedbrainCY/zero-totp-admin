42263e33a272be0cc9853e4c78bc4bbc7f2ace7f
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-25 21:26:43 -0400

    Improve tools code structure to not run on import

--- api/tools/regenerate_root_password.py
+++ api/tools/regenerate_root_password.py
@@ -8,32 +8,35 @@ from hashlib import scrypt
 from base64 import b64encode
 from uuid import uuid4
 
+def reset():
+    print("########################")
+    print("Regenerate root password")
+    print("########################\n")
+    password = getpass(prompt="New root password: ")
+    confirm_password = getpass(prompt="Confirm new root password: ")
+    salt = os.urandom(16)
+    hashed_pass = b64encode(scrypt(password.encode(), salt=salt, n=conf.security.scrypt_n, r=conf.security.
+    scrypt_r, p=conf.security.scrypt_p))
+    salt_b64 = b64encode(salt)
+    if password != confirm_password:
+        print("Passwords do not match")
+        exit(1)
 
-print("########################")
-print("Regenerate root password")
-print("########################\n")
-password = getpass(prompt="New root password: ")
-confirm_password = getpass(prompt="Confirm new root password: ")
-salt = os.urandom(16)
-hashed_pass = b64encode(scrypt(password.encode(), salt=salt, n=conf.security.scrypt_n, r=conf.security.
-scrypt_r, p=conf.security.scrypt_p))
-salt_b64 = b64encode(salt)
-if password != confirm_password:
-    print("Passwords do not match")
-    exit(1)
+    engine = create_engine(conf.database.zero_totp_admin_uri)
+    with Session(engine) as session:
+        admin = session.query(Admin).filter(Admin.username == "root").first()
+        if admin:
+            admin.password = hashed_pass
+            admin.password_salt = salt_b64
+            session.commit()
+        else:
+            admin = Admin(id=str(uuid4()), username="root", password=hashed_pass, password_salt=salt_b64)
+            
+            session.add(admin)
+            session.commit()
 
+    print("Root password updated successfully")
+    print("Thanks for using Zero-TOTP!")
 
-engine = create_engine(conf.database.zero_totp_admin_uri)
-with Session(engine) as session:
-    admin = session.query(Admin).filter(Admin.username == "root").first()
-    if admin:
-        admin.password = hashed_pass
-        admin.password_salt = salt_b64
-        session.commit()
-    else:
-        admin = Admin(id=str(uuid4()), username="root", password=hashed_pass, password_salt=salt_b64)
-        session.add(admin)
-        session.commit()
-
-print("Root password updated successfully")
-print("Thanks for using Zero-TOTP!")
+if __name__ == "__main__":
+    reset()
