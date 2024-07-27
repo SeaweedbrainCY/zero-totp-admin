92189e811f347d6ccc4db1581f77682f0ccf5463
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-27 18:58:16 -0400

    Add support of users retrieval and display

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,17 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faUsers, faUserLock, faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
-
+import { HttpClient } from '@angular/common/http';
+import { Utils } from '../common/Utils/utils.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
   faUsers = faUsers;
   faUserLock = faUserLock;
   faEye = faEye;
   faTrash = faTrash;
-  user_info_modal = true;
+  user_info_modal = false;
+  users: any[] = [];
 
-  constructor() { }
+ 
+  constructor(
+    private http: HttpClient,
+    private utils: Utils,
+    private toastr: ToastrService,
+    private router:Router
+  ) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.verifyAuthentication().then((isAuthenticated) => {
+      if(isAuthenticated) {
+        this.get_all_users();
+      }
+    });
+  }
+
+  private verifyAuthentication(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.http.get("/api/v1/whoami",  {withCredentials:true, observe: 'response'}).subscribe((response) => {
+        if (response.status === 200) {
+          resolve(true);
+        }
+    }, (error) => {
+      if(error.status === 401) {
+        this.redirectToLogin();
+        resolve(false);
+      } else {
+          console.error(error);
+          this.utils.toastError(this.toastr, "Impossible to verify authentication", error.error.message)
+          this.redirectToLogin();
+          resolve(false);
+      }
+    });
+    });
+  }
+
+  private redirectToLogin() {
+
+    this.utils.toastError(this.toastr, "You are not authenticated", "")
+    this.router.navigate(['/login']);
+    
+  }
+
+  private get_all_users() {
+    this.http.get("/api/v1/users/all",  {withCredentials:true, observe: 'response'}).subscribe((response) => {
+      if (response.status === 200) {
+        const body = JSON.parse(JSON.stringify(response.body));
+        console.log(body.users)
+        this.users = body.users;
+      }
+     
+  }, (error) => {
+    if(error.status === 401) {
+      this.redirectToLogin();
+      return;
+    } else {
+      console.error(error);
+      this.utils.toastError(this.toastr, "Impossible to get users", error.error.message)
+    }
+  });
+  }
 }
