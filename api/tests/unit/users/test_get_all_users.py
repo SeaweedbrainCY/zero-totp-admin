b5ff405dd72e6873b29950dd074dde2fb75f11b7
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-27 18:33:15 -0400

    Add coverage of get all uers

---
+++ api/tests/unit/users/test_get_all_users.py
@@ -0,0 +1,108 @@
+import unittest
+from main import app,db
+from unittest.mock import patch
+from zero_totp_db_model.model import User
+from environment.configuration import conf
+from admin_database.models import Admin as AdminModel, Session as SessionModel
+from uuid import uuid4
+import datetime as dt
+import json
+
+class TestGetAllUsers(unittest.TestCase):
+    def setUp(self):
+        if conf.database.zero_totp_admin_uri != "sqlite:///:memory:" or conf.database.  zero_totp_db_uri != "sqlite:///:memory:":
+                raise Exception(f"Test must be run with in memory database. Admin URI: {conf.database.zero_totp_admin_uri} User URI: {conf.database.zero_totp_db_uri}")
+        self.application = app.app
+        self.client = app.test_client()
+        self.endpoint = "/api/v1/users/all"
+        self.session_id = str(uuid4())
+
+        self.users_info = [
+            {
+             "id":2,
+             "username":"user2",
+             "email":"users2@mail.com",
+             "signup_date":"2021-10-10",
+            "isVerified":True,
+            "isBlocked":False
+            },
+            {
+             "id":1,
+             "username":"user1",
+             "email":"users1@mail.com",
+             "signup_date":"2022-10-10",
+            "isVerified":True,
+            "isBlocked":True
+            },
+            {
+             "id":22,
+             "username":"user22",
+             "email":"users22@mail.com",
+             "signup_date":"2021-12-10",
+            "isVerified":True,
+            "isBlocked":False
+            },
+            {
+             "id":90,
+             "username":"user90",
+             "email":"users90@mail.com",
+             "signup_date":"2021-10-31",
+            "isVerified":False,
+            "isBlocked":False
+            },
+            {
+             "id":62,
+             "username":"user62",
+             "email":"users62@mail.com",
+             "signup_date":"2024-10-10",
+            "isVerified":False,
+            "isBlocked":True
+            },
+            {
+             "id":20,
+             "username":"user20",
+             "email":"users20@mail.com",
+             "signup_date":"2021-20-10",
+            "isVerified":True,
+            "isBlocked":False
+            },
+
+        ]
+        with self.application.app_context():
+            db.create_all()
+            for user in self.users_info:
+                user = User(id=user["id"], username=user["username"], mail=user["email"], password="random", passphraseSalt="doesn't matter", isVerified=user["isVerified"], derivedKeySalt="doesn't matter", createdAt=user["signup_date"], isBlocked=user["isBlocked"])
+                db.session.add(user)
+
+            admin = AdminModel(id=str(uuid4()), username="root", password="random", password_salt="doesn't matter")
+            session = SessionModel(id=self.session_id, user_id=admin.id, expiration=dt.datetime.now(dt.UTC).timestamp() + 3600)
+            db.session.add(admin)
+            db.session.add(session)
+            db.session.commit()
+    
+    def tearDown(self):
+        with self.application.app_context():
+            db.session.remove()
+            db.drop_all()
+            patch.stopall()
+
+    
+
+    def test_get_user_by_id(self):
+        with self.application.app_context():
+            self.client.cookies = {"session_id": self.session_id}
+            response = self.client.get(self.endpoint)
+            self.assertEqual(response.status_code, 200)
+            returned_users = response.json()["users"]
+            for user in self.users_info:
+                infos = {}
+                for returned_user in returned_users:
+                    if returned_user["id"] == user["id"]:
+                        infos = returned_user
+                        break
+                self.assertNotEqual(infos, {}, f"User not found. Was looking for user with id {user['id']} in {returned_users}")
+                for key in user.keys():
+                    self.assertEqual(infos[key], user[key])
+    
+
+
