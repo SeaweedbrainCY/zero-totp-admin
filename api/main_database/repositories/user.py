741fbc668c370ce43408a35b206a59edd09559ed
seaweedbrain <nathanstchepinsky@gmail.com>
2024-08-17 00:18:23 +0200

    Fix deletion repo

--- api/main_database/repositories/user.py
+++ api/main_database/repositories/user.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from zero_totp_db_model.model import User
+from zero_totp_db_model.model import User, ZKE_encryption_key,TOTP_secret,GoogleDriveIntegration,Preferences,EmailVerificationToken,RateLimiting
 from main import db
 
 
@@ -18,9 +18,16 @@ def update_blocked_status_by_userid(user_id, is_blocked):
     return user
 
 def delete_user_by_id(user_id):
+
     user = db.session.query(User).filter(User.id == user_id).first()
     if not user:
         return None
+    db.session.query(ZKE_encryption_key).filter(ZKE_encryption_key.user_id == user_id).delete()
+    db.session.query(TOTP_secret).filter(TOTP_secret.user_id == user_id).delete()
+    db.session.query(GoogleDriveIntegration).filter(GoogleDriveIntegration.user_id == user_id).delete()
+    db.session.query(Preferences).filter(Preferences.user_id == user_id).delete()
+    db.session.query(EmailVerificationToken).filter(EmailVerificationToken.user_id == user_id).delete()
+    db.session.query(RateLimiting).filter(RateLimiting.user_id == user_id).delete()
     db.session.delete(user)
     db.session.commit()
     return True
