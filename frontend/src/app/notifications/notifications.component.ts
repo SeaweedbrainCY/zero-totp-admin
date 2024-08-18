d275933acb834e5a59100ea2e3e7554a5a9f8076
seaweedbrain <nathanstchepinsky@gmail.com>
2024-08-18 11:47:12 +0200

    add start of notification

--- frontend/src/app/notifications/notifications.component.ts
+++ frontend/src/app/notifications/notifications.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
+import { Utils } from '../common/Utils/utils.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-notifications',
-  standalone: true,
-  imports: [],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit {
+  faBell=faBell;
 
+  constructor(
+    private http: HttpClient,
+    private utils: Utils,
+    private router:Router,
+    private toastr: ToastrService
+  ) { }
+  
+  
+  ngOnInit(): void {
+    this.verifyAuthentication().then((isAuthenticated) => {
+      if(isAuthenticated) {
+        console.log("Authenticated");
+      }});
+  }
+
+
+  private verifyAuthentication(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.http.get("/api/v1/whoami",  {withCredentials:true, observe: 'response'}).subscribe((response) => {
+        if (response.status === 200) {
+          resolve(true);
+        }
+    }, (error) => {
+      if(error.status === 401) {
+        this.redirectToLogin();
+        resolve(false);
+      } else {
+          console.error(error);
+          this.utils.toastError(this.toastr, "Impossible to verify authentication", error.error.message)
+          this.redirectToLogin();
+          resolve(false);
+      }
+    });
+    });
+  }
+  private redirectToLogin() {
+
+    this.utils.toastError(this.toastr, "You are not authenticated", "")
+    this.router.navigate(['/login']);
+    
+  }
 }
