f745115d0d350bfabe0b0bb5ed6c89c3f2918e59
seaweedbrain <nathanstchepinsky@gmail.com>
2024-08-19 23:46:59 +0200

    Add GET, PUT, POST, DELETE and get all notification routes and controllers placeholder

---
+++ api/controllers/notifications.py
@@ -0,0 +1,21 @@
+from flask import request, Response
+import os
+from main_database.repositories import user as user_repo
+from base64 import b64encode, b64decode
+from environment.configuration import conf, logging
+import json
+
+def get_notification_by_id(notif_id):
+    pass 
+
+def update_notification(notif_id):
+    pass
+
+def delete_notification(notif_id):
+    pass
+
+def get_all_notifications():
+    pass
+
+def create_notification():
+    pass
