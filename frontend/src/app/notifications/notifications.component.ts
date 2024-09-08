e4215490e67534ddbef9051d4e8e8109f9c6ee89
seaweedbrain <nathanstchepinsky@gmail.com>
2024-09-08 18:49:38 -0400

    Add support of get all and post notification

--- frontend/src/app/notifications/notifications.component.ts
+++ frontend/src/app/notifications/notifications.component.ts
@@ -4,7 +4,15 @@ import { HttpClient } from '@angular/common/http';
 import { Utils } from '../common/Utils/utils.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
+
+interface notification_data {
+  message:string,
+  expiration_timestamp_utc?:number,
+  auth_user_only?:boolean,
+  enabled?:boolean
+}
 
 @Component({
   selector: 'app-notifications',
@@ -13,27 +21,49 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NotificationsComponent implements OnInit {
   faBell=faBell;
-  expiration: string | undefined;
+  notifExpiration: string | undefined;
   faCheckToSlot=faCheckToSlot;
   faUserCheck=faUserCheck;
   notif_enabled: boolean = false;
   notif_auth_user_only: boolean = false;
   edit_notif_uuid: string | undefined;
+  notifications: notification_data[] = [];
+  notifMessage= "";
+  localTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  displayed_notification: notification_data | undefined;
+
 
   constructor(
     private http: HttpClient,
     private utils: Utils,
     private router:Router,
     private toastr: ToastrService,
+    private translate: TranslateService
     
   ) { }
+
+  
   
   
   ngOnInit(): void {
     this.verifyAuthentication().then((isAuthenticated) => {
       if(isAuthenticated) {
-        console.log("Authenticated");
+        this.getAllNotifcations();
       }});
+  }
+
+  private getAllNotifcations(){
+    this.http.get("/api/v1/notifications/all",  {withCredentials:true, observe: 'response'}).subscribe((response) => {
+      if (response.status === 200) {
+        const body = JSON.parse(JSON.stringify(response.body));
+        this.notifications = body.notifications;
+      }
+  }, (error) => {
+    if(error.status != 404) {
+        console.error(error);
+        this.utils.toastError(this.toastr, "Impossible to retrieve authentications. ", error.message)
+    }
+  });
   }
 
 
@@ -67,7 +97,43 @@ export class NotificationsComponent implements OnInit {
   }
 
   public saveNotificationConfig(){
-    
+    if(this.notifMessage == ""){
+      this.translate.get("notifications.errors.empty").subscribe((translation)=>{
+        this.utils.toastError(this.toastr, translation, "")
+        return;
+      });
+    } else {
+      let body: notification_data = {
+        "message": this.notifMessage,
+        "auth_user_only": this.notif_auth_user_only,
+        "enabled": this.notif_enabled
+      }
+      console.log(this.notifExpiration);
+      if(this.notifExpiration != undefined && this.notifExpiration != "" && this.notifExpiration != null){
+        const expiration_utc = new Date(this.notifExpiration).getTime();
+        const unix_timestamp = expiration_utc / 1000;
+        body["expiration_timestamp_utc"] = unix_timestamp;
+      }
+      this.http.post("/api/v1/notification", body, {withCredentials:true, observe: 'response'}).subscribe((response) => {
+        if (response.status === 201) {
+          const body = JSON.parse(JSON.stringify(response.body));
+          this.router.navigate(['/notifications/' + body.id]);
+        } else {
+          this.translate.get("notifications.errors.save").subscribe((translation)=>{
+            this.utils.toastError(this.toastr, translation, "")
+          });
+        }
+      }, (error) => {
+        if(error.status === 401) {
+          this.redirectToLogin();
+        } else if (error.status === 400){
+          this.utils.toastError(this.toastr, "Impossible to save this user", error.error.error)
+        } else {
+            console.error(error);
+            this.utils.toastError(this.toastr, "Impossible to save this user", error.message)
+        }
+      });
+    }
   }
 
   public isNotificationDisplayed(uuid:string){
