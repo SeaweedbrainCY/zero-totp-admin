b3a00760eb6b89371e6a37586e93b3121c937e26
seaweedbrain <nathanstchepinsky@gmail.com>
2024-09-08 14:27:35 -0400

    Add create notif repo

--- api/main_database/repositories/notifications.py
+++ api/main_database/repositories/notifications.py
@@ -1,5 +1,15 @@
 from zero_totp_db_model.model import Notifications
 from main import db
+import datetime as dt
+from uuid import uuid4
 
 def get_notification_by_id(notif_id):
     return db.session.query(Notifications).filter_by(id=notif_id).first()
+
+def create_notification(message, expiry=None, enabled=False, authenticated_user_only=False):
+    timestamp = str(dt.datetime.now(dt.UTC).timestamp())
+    id = str(uuid4())
+    notif = Notifications(id=id, timestamp=timestamp, message=message, expiry=expiry, enabled=enabled, authenticated_user_only=authenticated_user_only)
+    db.session.add(notif)
+    db.session.commit()
+    return notif
