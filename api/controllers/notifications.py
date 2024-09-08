6537c9318ca234cec7b4b570893b2ee2040a1c99
seaweedbrain <nathanstchepinsky@gmail.com>
2024-09-08 11:36:13 -0400

    Add controller to get notif

--- api/controllers/notifications.py
+++ api/controllers/notifications.py
@@ -1,12 +1,23 @@
 from flask import request, Response
 import os
-from main_database.repositories import user as user_repo
+from main_database.repositories import notifications as notif_repo
 from base64 import b64encode, b64decode
 from environment.configuration import conf, logging
 import json
 
 def get_notification_by_id(notif_id):
-    pass 
+    notif = notif_repo.get_notification_by_id(notif_id)  
+    if not notif:
+        return {"error": "Notification not found"}, 404
+    return {
+        "id": notif.id,
+        "message": notif.message,
+        "timestamp": notif.timestamp,
+        "expiration_timestamp": notif.expiry,
+        "enabled": notif.enabled,
+        "auth_user_only": notif. authenticated_user_only
+    }, 200
+    
 
 def update_notification(notif_id):
     pass
